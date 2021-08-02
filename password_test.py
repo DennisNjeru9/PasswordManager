cc6768a6f176d5686a18a78d97c03402292b5b88
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-02 21:11:23 +0300

    creates unittest.TestCase to help in building tests for the User class behaviours.

---
+++ password_test.py
@@ -0,0 +1,12 @@
+import unittest #Importimg the unittest module
+from password import User #Importing the User class
+from password import Credentials #Importing the Credentials class
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the User class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that aids in creating test cases.
+    '''
+
