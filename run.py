76cbe8512f2fb746567156b79e9ba30e7614e475
DennisNjeru9 <dennis.njeri@student.moringaschool.com>
2021-08-03 12:34:26 +0300

    add functions to create new user accounts and credentials for different accounts.

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3.8
+from password import User
+from password import Credentials
+
+#Create an account
+def create_user(username,password):
+    '''
+    Function to create a new user account.
+    '''
+
+    new_user = User(username,password)
+    return new_user
+
+
+#Create a new credential
+def create_credentials(aname,ausername,apassword):
+    '''
+    Function to create a new credential.
+    '''
+
+    new_credentials = Credentials(aname,ausername,apassword)
+    return new_credentials
+
+
